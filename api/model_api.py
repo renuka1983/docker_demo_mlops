a377928e0e107accd92ae77ec806e5dc45c926e1
renuka <renu.stat@gmail.com>
2025-11-09 09:41:57 +0530

    Changes upated

--- api/model_api.py
+++ api/model_api.py
@@ -7,12 +7,12 @@ model = pickle.load(open("models/log_model.sav","rb"))
 
 class Input(BaseModel):
     Pregnancies:int; Glucose:int; BloodPressure:int; SkinThickness:int
-    Insulin:int; BMI:float; DiabetesPedigreeFunction:float; Age:int
+    Insulin:int; DiabetesPedigreeFunction:float; BMI_Category:int;Age_Group:int ; 
 
 @app.post("/diabetes_prediction")
 def predict(inp: Input):
     arr = np.array([[inp.Pregnancies, inp.Glucose, inp.BloodPressure,
-                     inp.SkinThickness, inp.Insulin, inp.BMI,
-                     inp.DiabetesPedigreeFunction, inp.Age]])
+                     inp.SkinThickness, inp.Insulin, 
+                     inp.DiabetesPedigreeFunction,inp.BMI_Category, inp.Age_Group]])
     y = model.predict(arr)[0]
     return {"prediction": "Patient is diabetic" if y==1 else "Patient is not diabetic"}
--- feature_store/features_pipeline.py
+++ feature_store/features_pipeline.py
@@ -6,9 +6,12 @@ df["BMI_Category"] = pd.cut(df["BMI"], [0,18.5,25,30,100],
                             labels=["Underweight","Normal","Overweight","Obese"])
 df["Age_Group"] = pd.cut(df["Age"], [0,30,50,100],
                          labels=["Young","Middle","Senior"])
+df.drop[['BMI','Age'],axis = 1,inplace = True ]
 
 conn = sqlite3.connect("feature_store/feature_store.db")
-df.to_sql("diabetes_features", conn, if_exists="replace", index=False)
+# we can put only the selected columns into the featrue stere df_selected= df['']
+df.to_sql("diabetes_features", conn, if_exists="replace", index=False) 
+# entire the rows and columns are stored in the database table named diabetes_feature
 conn.close()
 
 meta = {"dataset":"diabetes_features","features":list(df.columns),"num_records":len(df)}
